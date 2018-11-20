fc9dd1fb55925baf3a292fedc42732ded33c1490
Jonathan Hernandez Zapata <jonathan.hernandez@e-hps.com>
2018-11-20 18:17:38 -0500

    commit

--- my-app.js
+++ my-app.js
@@ -1,4 +1,11 @@
-var result = (x, y) => {
-    return x > y;
-}
-module.exports = result;
+const add = (x, y) => x + y;
+const subtract = (x, y) => x - y;
+const multiply = (x, y) => x * y;
+const divide = (x, y) => x / y;
+
+module.exports = {
+    add : add,
+    subtract : subtract,
+    multiply : multiply,
+    divide : divide,
+};
--- spec/my-app.spec.js
+++ spec/my-app.spec.js
@@ -1,6 +1,80 @@
-var comparer = require("../my-app");
-describe("Brief description of the Test Suite", () => {
-    xit("Docstring for each test", () => {
-        expect(comparer(2, 3)).toEqual(false);
+var math = require("../my-app");
+describe("Math features", () => {
+    
+    describe("add feature", () => {
+        let numberOne;
+        let numberTwo;
+
+        beforeEach(() => {
+            numberOne = 2;
+            numberTwo = 3;
+        });
+
+        it('case one', () => {
+            expect(math.add(numberOne, numberTwo)).toEqual(5);
+        });
+
+        it('case one', () => {
+            numberOne = -1;
+            expect(math.add(numberOne, numberTwo)).toEqual(2);
+        });
+    })
+    
+    describe("subtract feature", () => {
+        let numberOne;
+        let numberTwo;
+
+        beforeEach(() => {
+            numberOne = 2;
+            numberTwo = 3;
+        });
+
+        it('case one', () => {
+            expect(math.subtract(numberTwo, numberOne)).toEqual(1);
+        });
+
+        it('case one', () => {
+            numberOne = -1;
+            expect(math.subtract(numberOne, numberTwo)).toEqual(-4);
+        });
+    })
+
+    describe("multiply feature", () => {
+        let numberOne;
+        let numberTwo;
+
+        beforeEach(() => {
+            numberOne = 5;
+            numberTwo = 1;
+        });
+
+        it('case one', () => {
+            expect(math.multiply(numberTwo, numberOne)).toEqual(numberOne);
+        });
+
+        it('case one', () => {
+            numberTwo = 10;
+            expect(math.multiply(numberOne, numberTwo)).toEqual(50);
+        });
+    })
+
+    describe("divide feature", () => {
+        let numberOne;
+        let numberTwo;
+
+        beforeEach(() => {
+            numberOne = 5;
+            numberTwo = 1;
+        });
+
+        it('case one', () => {
+            expect(math.divide(numberOne, numberTwo)).toEqual(numberOne);
+        });
+
+        it('case one', () => {
+            numberOne = 10;
+            numberTwo = 2;
+            expect(math.divide(numberOne, numberTwo)).toEqual(5);
+        });
     })
 });
