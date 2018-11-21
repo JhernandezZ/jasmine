7d03db788f66fef43490678f90fcc75c45ed6d75
Jonathan Hernandez Zapata <jonathan.hernandez@e-hps.com>
2018-11-21 08:18:15 -0500

    it was added a new one reporter. It was completed the exercise one

---
+++ spec/divide.spec.js
@@ -0,0 +1,27 @@
+var math = require("../my-app");
+describe("divide feature", () => {
+    let numberOne;
+    let numberTwo;
+
+    beforeEach(() => {
+        numberOne = 5;
+        numberTwo = 1;
+    });
+
+    it('case one', () => {
+        expect(math.divide(numberOne, numberTwo)).toEqual(numberOne);
+    });
+
+    it('case two', () => {
+        numberOne = 10;
+        numberTwo = 2;
+        expect(math.divide(numberOne, numberTwo)).toEqual(5);
+    });
+    
+    it('case three', () => {
+        numberOne = 10;
+        numberTwo = 0;
+        const result = 'The second number should be different of zero';
+        expect(math.divide(numberOne, numberTwo)).toEqual(result);
+    });
+})
---
+++ spec/subtract.spec.js
@@ -0,0 +1,20 @@
+var math = require("../my-app");
+
+describe("subtract feature", () => {
+    let numberOne;
+    let numberTwo;
+
+    beforeEach(() => {
+        numberOne = 2;
+        numberTwo = 3;
+    });
+
+    it('case one', () => {
+        expect(math.subtract(numberTwo, numberOne)).toEqual(1);
+    });
+
+    it('case two', () => {
+        numberOne = -1;
+        expect(math.subtract(numberOne, numberTwo)).toEqual(-4);
+    });
+})
